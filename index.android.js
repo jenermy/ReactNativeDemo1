f485294123afb31af68d18e8eb2f3040b3cdeea0
wanlijun <1531593849@qq.com>
2018-04-20 11:32:27 +0800

    react native ，页面跳转，页面数据刷新，数据传递，简单的小demo

---
+++ index.android.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import PropTypeScreen from '.js/PropTypeScreen';
+
+AppRegistry.registerComponent('RNHelloWorld', () => PropTypeScreen);
---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import PropTypeScreen from './js/PropTypeScreen.js';
+
+AppRegistry.registerComponent('RNHelloWorld', () => PropTypeScreen);
